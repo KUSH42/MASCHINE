0a57edcfb03538704776f531824b6963e37c63fe
Kush <Kush@DESKTOP-IKNT2DM>
2016-09-24 07:18:17 +0200

    input handling

--- MASCHINE-core/src/graphics/window.cpp
+++ MASCHINE-core/src/graphics/window.cpp
@@ -2,7 +2,7 @@
 
 namespace MASCHINE { namespace graphics {
 
-	void windowResize(GLFWwindow *window, int width, int height);
+	void window_resize(GLFWwindow *window, int width, int height);
 
 	Window::Window(const char *title, int width, int height)
 	{
@@ -12,6 +12,10 @@ namespace MASCHINE { namespace graphics {
 		if (!init())
 			glfwTerminate();
 		
+		for (int i = 0; i < MAX_KEYS; i++)
+			m_Keys[i] = false;
+		for (int i = 0; i < MAX_BUTTONS; i++)
+			m_Buttons[i] = false;
 	}
 
 	Window::~Window()
@@ -40,7 +44,11 @@ namespace MASCHINE { namespace graphics {
 			return false;
 		}
 		glfwMakeContextCurrent(m_Window);			//Establish context before initializing GLEW
-		glfwSetWindowSizeCallback(m_Window, windowResize);
+		glfwSetWindowUserPointer(m_Window, this);
+		glfwSetWindowSizeCallback(m_Window, window_resize);
+		glfwSetKeyCallback(m_Window, key_callback);
+		glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
+		glfwSetCursorPosCallback(m_Window, cursor_position_callback);
 
 		if (glewInit() != GLEW_OK)
 		{
@@ -63,9 +71,52 @@ namespace MASCHINE { namespace graphics {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
-	void windowResize(GLFWwindow *window, int width, int height)
+	void window_resize(GLFWwindow *window, int width, int height)
 	{
 		glViewport(0, 0, width, height);
+	}
+
+	void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+	{
+		MASCHINE::graphics::Window* win = (MASCHINE::graphics::Window*) glfwGetWindowUserPointer(window);
+		win->m_Keys[key] = (action != GLFW_RELEASE);
+	}
+
+	void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
+	{
+		MASCHINE::graphics::Window* win = (MASCHINE::graphics::Window*) glfwGetWindowUserPointer(window);
+		win->m_Buttons[button] = (action != GLFW_RELEASE);
+	}
+
+	void cursor_position_callback(GLFWwindow *window, double xpos, double ypos)
+	{
+		MASCHINE::graphics::Window* win = (MASCHINE::graphics::Window*) glfwGetWindowUserPointer(window);
+		win->mouse_x = xpos;
+		win->mouse_y = ypos;
+	}
+
+	void Window::getMousePosition(double& x, double& y) const
+	{
+		x = mouse_x;
+		y = mouse_y;
+	}
+
+	bool Window::isKeyPressed(unsigned int keycode) const
+	{
+		//TODO: Logging system
+		if (keycode >= MAX_KEYS)
+			return false;
+
+		return m_Keys[keycode];
+	}
+
+	bool Window::isMouseButtonPressed(unsigned int button) const
+	{
+		//TODO: Logging system
+		if (button >= MAX_BUTTONS)
+			return false;
+
+		return m_Buttons[button];
 	}
 
 } }
